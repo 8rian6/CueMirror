085e6358cda10e01cba47a759310a735925d60c1
8rian6 <uppcr@msn.com>
2026-07-21 14:47:59 +0800

    Initial Commit

---
+++ CueMirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CueMirror
+//
+//  Created by Bri on 21/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CueMirror/CueMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueMirrorApp.swift
+//  CueMirror
+//
+//  Created by Bri on 21/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct CueMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
